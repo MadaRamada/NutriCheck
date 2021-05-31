1bf973da6012febde7471e9fb83e19961dd0ab0e
mada <handiyanto.ramadaaji@gmail.com>
2021-06-01 01:35:37 +0800

    add nutrition

--- scripts.js
+++ scripts.js
@@ -18,29 +18,41 @@ async function sendApiRequest() {
   let api = await fetch(`https://api.edamam.com/search?app_id=${app_id}&app_key=${app_key}&q=${query}`)
   let data = await api.json()
   useApiData(data)
+  console.log(data)
 }
 
 function useApiData(data) {
   let output = ''
   for (let i = 0; i < data.hits.length; i++) {
     output = output + `
-    <div class="col-lg-3 col-md-4 col-sm-6 mt-5">
+    <div class="col-lg-3 col-md-4 col-sm-6 mt-5" data-toggle="tooltip" data-html="true" title="
+    Nutrition : 
+    Carbs : ${Math.round(data.hits[1].recipe.totalNutrients.CHOCDF.quantity / data.hits[i].recipe.yield)} g
+    Cholesterol : ${Math.round(data.hits[1].recipe.totalNutrients.CHOLE.quantity / data.hits[i].recipe.yield)} mg
+    Fat : ${Math.round(data.hits[1].recipe.totalNutrients.FAT.quantity / data.hits[i].recipe.yield)} g
+    Protein : ${Math.round(data.hits[1].recipe.totalNutrients.PROCNT.quantity / data.hits[i].recipe.yield)} g
+    ">
       <div class="testimonial-item mx-auto">
-        <img class="img-fluid rounded mb-3" src="${data.hits[i].recipe.image}" alt="..." />
+        <img class="img-fluid rounded mb-3" src="${data.hits[i].recipe.image}" alt="image" />
         <a href="${data.hits[i].recipe.url}"><h5>${data.hits[i].recipe.label}</h5></a>
-        <p class="font-weight-light mb-0"><b>${Math.round((data.hits[i].recipe.calories) / data.hits[i].recipe.yield)} kcal/serving</b></p>
+        <p class="font-weight-light mb-0"><b>${Math.round(data.hits[i].recipe.calories / data.hits[i].recipe.yield)} kcal/serving</b></p>
+  
       </div>
     </div>
     `
   }
   if (data.hits.length == 0) {
     document.querySelector("#content").innerHTML = `
-    <h2 class="mb-5">Recipe not found . . .</h2>
+    <h2 class="mb-5">Recipe not found</h2>
     `
   } else {
     document.querySelector("#content").innerHTML = `
-    <h2 class="mb-5">Here are your results . . .</h2>
+    <h2 class="mb-5">Here are your results</h2>
     <div class="row">${output}</div>
     `
   }
 }
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
